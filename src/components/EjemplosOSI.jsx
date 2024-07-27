1e856fed69c6fb0cbe6486e2f2d103faa3ac26bb
Kenny Saenz <ksaenz@bi.com.gt>
2024-07-27 00:17:57 -0600

    Auto stash before checking out "origin/development"

--- src/components/EjemplosOSI.jsx
+++ src/components/EjemplosOSI.jsx
@@ -1,34 +1,35 @@
 import React, { useState } from "react";
 import { CSSTransition } from "react-transition-group";
 import "./transitions.css"; 
+
 const ejemplosPorCapaOSI = {
   "APLICACIÓN": [
-    { src: "/pop3.webp", alt: "POP3", definition: "POP3 es un protocolo de correo electrónico usado para descargar correos desde un servidor remoto.", reason: "Pertenece a la capa de aplicación porque se encarga de la gestión del correo electrónico a nivel de usuario.", useCases: "Clientes de correo electrónico." },
-    { src: "/navegador.png", alt: "Navegador web", definition: "Un navegador web es una aplicación utilizada para acceder a sitios web y contenido en la web.", reason: "Pertenece a la capa de aplicación porque facilita la interacción del usuario con los sitios web.", useCases: "Navegadores web como Chrome, Firefox." },
+    { src: "/pop3.webp", alt: "POP3", definition: "POP3 es un protocolo de correo electrónico usado para descargar correos desde un servidor remoto.", reason: "Pertenece a la capa de aplicación porque se encarga de la gestión del correo electrónico a nivel de usuario.", useCases: "Clientes de correo electrónico.", ports: "Puerto 110" },
+    { src: "/navegador.png", alt: "Navegador web", definition: "Un navegador web es una aplicación utilizada para acceder a sitios web y contenido en la web.", reason: "Pertenece a la capa de aplicación porque facilita la interacción del usuario con los sitios web.", useCases: "Navegadores web como Chrome, Firefox.", ports: "Puerto 80 (HTTP), Puerto 443 (HTTPS)" },
   ],
   "PRESENTACIÓN": [
-    { src: "/mpeg.webp", alt: "MPEG", definition: "MPEG o JPG son formatos de compresión de imagenes y  video utilizado para el almacenamiento y transmisión de video digital.", reason: "Pertenece a la capa de presentación porque se encarga de la codificación y compresión de datos.", useCases: "Reproductores de video, streaming, imagenes." },
-    { src: "/ssl.png", alt: "SSL", definition: "SSL es un protocolo de seguridad que proporciona comunicaciones seguras a través de una red informática.", reason: "Pertenece a la capa de presentación porque se encarga del cifrado de datos para una transmisión segura.", useCases: "Transacciones seguras en línea." },
+    { src: "/mpeg.png", alt: "MPEG", definition: "MPEG es un formato de compresión de video utilizado para el almacenamiento y transmisión de video digital.", reason: "Pertenece a la capa de presentación porque se encarga de la codificación y compresión de datos.", useCases: "Reproductores de video, streaming.", ports: "Puerto no aplicable" },
+    { src: "/ssl.png", alt: "SSL", definition: "SSL es un protocolo de seguridad que proporciona comunicaciones seguras a través de una red informática.", reason: "Pertenece a la capa de presentación porque se encarga del cifrado de datos para una transmisión segura.", useCases: "Transacciones seguras en línea.", ports: "Puerto 443 (HTTPS)" },
   ],
   "SESIÓN": [
-    { src: "/pptp.webp", alt: "PPTP", definition: "PPTP es un protocolo de túnel de punto a punto que permite transferencias seguras de datos a través de redes públicas.", reason: "Pertenece a la capa de sesión porque establece y gestiona sesiones de comunicación entre dispositivos.", useCases: "Conexiones VPN." },
-    { src: "/rpc.png", alt: "RPC", definition: "RPC es un protocolo que un programa puede usar para solicitar un servicio de un programa ubicado en otro equipo en una red.", reason: "Pertenece a la capa de sesión porque permite la comunicación y gestión de tareas remotas.", useCases: "Servicios distribuidos." },
+    { src: "/pptp.webp", alt: "PPTP", definition: "PPTP es un protocolo de túnel de punto a punto que permite transferencias seguras de datos a través de redes públicas.", reason: "Pertenece a la capa de sesión porque establece y gestiona sesiones de comunicación entre dispositivos.", useCases: "Conexiones VPN.", ports: "Puerto 1723" },
+    { src: "/rpc.png", alt: "RPC", definition: "RPC es un protocolo que un programa puede usar para solicitar un servicio de un programa ubicado en otro equipo en una red.", reason: "Pertenece a la capa de sesión porque permite la comunicación y gestión de tareas remotas.", useCases: "Servicios distribuidos.", ports: "Puerto 135" },
   ],
   "TRANSPORTE": [
-    { src: "/tcp.gif", alt: "TCP", definition: "TCP es un protocolo de comunicación que permite la transmisión de datos de manera fiable entre dos dispositivos.", reason: "Pertenece a la capa de transporte porque garantiza la entrega correcta de datos entre dispositivos.", useCases: "Transmisión de datos, conexiones de red." },
-    { src: "/UDP-gif.gif", alt: "UDP", definition: "UDP es un protocolo de comunicación que permite la transmisión de datos sin establecer una conexión previa.", reason: "Pertenece a la capa de transporte porque permite la transmisión rápida de datos sin verificación.", useCases: "Transmisiones en vivo, juegos en línea." },
+    { src: "/tcp.gif", alt: "TCP", definition: "TCP es un protocolo de comunicación que permite la transmisión de datos de manera fiable entre dos dispositivos.", reason: "Pertenece a la capa de transporte porque garantiza la entrega correcta de datos entre dispositivos.", useCases: "Transmisión de datos, conexiones de red.", ports: "Puerto varía según la aplicación" },
+    { src: "/UDP-gif.gif", alt: "UDP", definition: "UDP es un protocolo de comunicación que permite la transmisión de datos sin establecer una conexión previa.", reason: "Pertenece a la capa de transporte porque permite la transmisión rápida de datos sin verificación.", useCases: "Transmisiones en vivo, juegos en línea.", ports: "Puert varía según la aplicación" },
   ],
   "RED": [
-    { src: "/IP.jpg", alt: "IP", definition: "IP es el protocolo principal de la capa de red que se encarga de la transmisión de paquetes de datos entre dispositivos.", reason: "Pertenece a la capa de red porque se encarga del direccionamiento y enrutamiento de paquetes.", useCases: "Enrutamiento de paquetes de datos." },
-    { src: "/icmp.png", alt: "ICMP", definition: "ICMP es un protocolo utilizado para enviar mensajes de control y error en la red.", reason: "Pertenece a la capa de red porque se utiliza para el diagnóstico y la gestión de errores de red.", useCases: "Ping, traceroute." },
+    { src: "/IP.jpg", alt: "IP", definition: "IP es el protocolo principal de la capa de red que se encarga de la transmisión de paquetes de datos entre dispositivos.", reason: "Pertenece a la capa de red porque se encarga del direccionamiento y enrutamiento de paquetes.", useCases: "Enrutamiento de paquetes de datos.", ports: "Puerto no aplicable" },
+    { src: "/icmp.png", alt: "ICMP", definition: "ICMP es un protocolo utilizado para enviar mensajes de control y error en la red.", reason: "Pertenece a la capa de red porque se utiliza para el diagnóstico y la gestión de errores de red.", useCases: "Ping, traceroute.", ports: "Puerto no aplicable" },
   ],
   "ENLACE DE DATOS": [
-    { src: "/ethernetswitch.jpg", alt: "Ethernet Switch", definition: "Un switch Ethernet es un dispositivo de red que conecta varios dispositivos en una red local (LAN).", reason: "Pertenece a la capa de enlace de datos porque gestiona la transferencia de datos entre dispositivos en una red local.", useCases: "Redes locales, comunicación de datos." },
-    { src: "/ppp.webp", alt: "PPP", definition: "PPP es un protocolo de enlace de datos utilizado para establecer una conexión directa entre dos nodos de red.", reason: "Pertenece a la capa de enlace de datos porque establece y configura conexiones de datos.", useCases: "Conexiones punto a punto." },
+    { src: "/ethernetswitch.jpg", alt: "Ethernet Switch", definition: "Un switch Ethernet es un dispositivo de red que conecta varios dispositivos en una red local (LAN).", reason: "Pertenece a la capa de enlace de datos porque gestiona la transferencia de datos entre dispositivos en una red local.", useCases: "Redes locales, comunicación de datos.", ports: "Puerto no aplicable" },
+    { src: "/ppp.webp", alt: "PPP", definition: "PPP es un protocolo de enlace de datos utilizado para establecer una conexión directa entre dos nodos de red.", reason: "Pertenece a la capa de enlace de datos porque establece y configura conexiones de datos.", useCases: "Conexiones punto a punto.", ports: " Puerto no aplicable" },
   ],
   "FISICA": [
-    { src: "/ethernet.webp", alt: "RJ", definition: "RJ es un tipo de conector físico utilizado en redes de telecomunicaciones y redes de datos.", reason: "Pertenece a la capa física porque define la interfaz física y los estándares eléctricos.", useCases: "Conectores de red, cables de telecomunicaciones." },
-    { src: "/fibrao.jpg", alt: "Fibra Óptica", definition: "La fibra óptica es una tecnología de transmisión que utiliza hilos de vidrio o plástico para transmitir datos a largas distancias.", reason: "Pertenece a la capa física porque se encarga de la transmisión de señales ópticas.", useCases: "Conexiones de alta velocidad, telecomunicaciones." },
+    { src: "/ethernet.webp", alt: "RJ", definition: "RJ es un tipo de conector físico utilizado en redes de telecomunicaciones y redes de datos.", reason: "Pertenece a la capa física porque define la interfaz física y los estándares eléctricos.", useCases: "Conectores de red, cables de telecomunicaciones.", ports: "No aplicable" },
+    { src: "/fibrao.jpg", alt: "Fibra Óptica", definition: "La fibra óptica es una tecnología de transmisión que utiliza hilos de vidrio o plástico para transmitir datos a largas distancias.", reason: "Pertenece a la capa física porque se encarga de la transmisión de señales ópticas.", useCases: "Conexiones de alta velocidad, telecomunicaciones.", ports: "Puerto no aplicable" },
   ],
 };
 
@@ -61,6 +62,7 @@ const EjemplosOSI = ({ layerTitle }) => {
                 <h2 className="text-xl font-bold mb-2">{image.alt}</h2>
                 <p className="mb-2">{image.definition}</p>
                 <p className="mb-2 font-bold">{image.reason}</p>
+                <p className="mb-2 text-xl">{image.ports}</p>
               </div>
             </div>
           </CSSTransition>
--- src/components/EjemplosTCPIP.jsx
+++ src/components/EjemplosTCPIP.jsx
@@ -4,25 +4,24 @@ import "./transitions.css";
 
 const ejemplosPorCapaTCPIP = {
   "APLICACIÓN": [
-    { src: "/http.jpeg", alt: "HTTP", definition: "HTTP es un protocolo de la capa de aplicación usado para la transmisión de información en la web.", reason: "Pertenece a la capa de aplicación porque facilita la comunicación entre aplicaciones web.", useCases: "Navegadores web, servidores web." },
-    { src: "/ftp.webp", alt: "FTP", definition: "FTP es un protocolo de red utilizado para la transferencia de archivos entre un cliente y un servidor.", reason: "Pertenece a la capa de aplicación porque se encarga de la gestión de archivos a nivel de usuario.", useCases: "Clientes FTP, servidores FTP." },
-    { src: "/dns.webp", alt: "DNS", definition: "DNS es un sistema que traduce nombres de dominio a direcciones IP.", reason: "Pertenece a la capa de aplicación porque facilita la resolución de nombres de dominio para aplicaciones.", useCases: "Servidores DNS." },
-    
+    { src: "/http.png", alt: "HTTP", definition: "HTTP es un protocolo de la capa de aplicación usado para la transmisión de información en la web.", reason: "Pertenece a la capa de aplicación porque facilita la comunicación entre aplicaciones web.", useCases: "Navegadores web, servidores web.", ports: "Puerto 80 (HTTP), Puerto 443 (HTTPS)" },
+    { src: "/ftp.png", alt: "FTP", definition: "FTP es un protocolo de red utilizado para la transferencia de archivos entre un cliente y un servidor.", reason: "Pertenece a la capa de aplicación porque se encarga de la gestión de archivos a nivel de usuario.", useCases: "Clientes FTP, servidores FTP.", ports: "Puerto 21" },
+    { src: "/dns.png", alt: "DNS", definition: "DNS es un sistema que traduce nombres de dominio a direcciones IP.", reason: "Pertenece a la capa de aplicación porque facilita la resolución de nombres de dominio para aplicaciones.", useCases: "Servidores DNS.", ports: "Puerto 53" },
   ],
   "TRANSPORTE": [
-    { src: "/tcp.gif", alt: "TCP", definition: "TCP es un protocolo de comunicación que permite la transmisión de datos de manera fiable entre dos dispositivos.", reason: "Pertenece a la capa de transporte porque garantiza la entrega correcta de datos entre dispositivos.", useCases: "Transmisión de datos, conexiones de red." },
-    { src: "/UDP-gif.gif", alt: "UDP", definition: "UDP es un protocolo de comunicación que permite la transmisión de datos sin establecer una conexión previa.", reason: "Pertenece a la capa de transporte porque permite la transmisión rápida de datos sin verificación.", useCases: "Transmisiones en vivo, juegos en línea." },
+    { src: "/tcp.png", alt: "TCP", definition: "TCP es un protocolo de comunicación que permite la transmisión de datos de manera fiable entre dos dispositivos.", reason: "Pertenece a la capa de transporte porque garantiza la entrega correcta de datos entre dispositivos.", useCases: "Transmisión de datos, conexiones de red.", ports: "Puerto varía según la aplicación" },
+    { src: "/udp.png", alt: "UDP", definition: "UDP es un protocolo de comunicación que permite la transmisión de datos sin establecer una conexión previa.", reason: "Pertenece a la capa de transporte porque permite la transmisión rápida de datos sin verificación.", useCases: "Transmisiones en vivo, juegos en línea.", ports: "Puerto varía según la aplicación" },
   ],
   "INTERNET": [
-    { src: "/ipv4.png", alt: "IPv4", definition: "IPv4 es una versión del Protocolo de Internet utilizado para identificar dispositivos en una red mediante una dirección IP.", reason: "Pertenece a la capa de internet porque gestiona el direccionamiento y enrutamiento de paquetes.", useCases: "Conexiones de red, enrutamiento de paquetes." },
-    { src: "/ipv6.jpg", alt: "IPv6", definition: "IPv6 es una versión más reciente del Protocolo de Internet con un mayor espacio de direcciones.", reason: "Pertenece a la capa de internet porque gestiona el direccionamiento y enrutamiento de paquetes con un espacio de direcciones ampliado.", useCases: "Conexiones de red, enrutamiento de paquetes." },
-    { src: "/bgp.png", alt: "BGP", definition: "BGP es un protocolo de enrutamiento utilizado para intercambiar información de enrutamiento entre sistemas autónomos en Internet.", reason: "Pertenece a la capa de internet porque gestiona el enrutamiento entre redes diferentes.", useCases: "Enrutamiento de Internet, gestión de redes." },
-    { src: "/icmp.png", alt: "ICMP", definition: "ICMP es un protocolo utilizado para enviar mensajes de control y error en la red.", reason: "Pertenece a la capa de internet porque se utiliza para diagnosticar problemas de red.", useCases: "Ping, traceroute." },
+    { src: "/ipv4.png", alt: "IPv4", definition: "IPv4 es una versión del Protocolo de Internet utilizado para identificar dispositivos en una red mediante una dirección IP.", reason: "Pertenece a la capa de internet porque gestiona el direccionamiento y enrutamiento de paquetes.", useCases: "Conexiones de red, enrutamiento de paquetes.", ports: "Puerto no aplicable" },
+    { src: "/ipv6.png", alt: "IPv6", definition: "IPv6 es una versión más reciente del Protocolo de Internet con un mayor espacio de direcciones.", reason: "Pertenece a la capa de internet porque gestiona el direccionamiento y enrutamiento de paquetes con un espacio de direcciones ampliado.", useCases: "Conexiones de red, enrutamiento de paquetes.", ports: "Puerto no aplicable" },
+    { src: "/bgp.png", alt: "BGP", definition: "BGP es un protocolo de enrutamiento utilizado para intercambiar información de enrutamiento entre sistemas autónomos en Internet.", reason: "Pertenece a la capa de internet porque gestiona el enrutamiento entre redes diferentes.", useCases: "Enrutamiento de Internet, gestión de redes.", ports: "Puerto 179" },
+    { src: "/icmp.png", alt: "ICMP", definition: "ICMP es un protocolo utilizado para enviar mensajes de control y error en la red.", reason: "Pertenece a la capa de internet porque se utiliza para diagnosticar problemas de red.", useCases: "Ping, traceroute.", ports: "Puerto no aplicable" },
   ],
   "ACCESO DE RED": [
-    { src: "/ethernet.webp", alt: "Ethernet", definition: "Ethernet es una tecnología de red utilizada para la comunicación de datos en redes locales (LAN).", reason: "Pertenece a la capa de acceso de red porque define la forma en que los dispositivos en una red local se comunican.", useCases: "Redes locales, comunicación de datos." },
-    { src: "/wifirouter.jpg", alt: "Wi-Fi", definition: "Wi-Fi es una tecnología de red inalámbrica que permite a los dispositivos comunicarse sin cables.", reason: "Pertenece a la capa de acceso de red porque define la comunicación inalámbrica en redes locales.", useCases: "Conexiones inalámbricas, redes domésticas." },
-    { src: "/dsl.jpg", alt: "DSL", definition: "DSL es una tecnología que proporciona acceso a Internet mediante la transmisión de datos digitales a través de líneas telefónicas.", reason: "Pertenece a la capa de acceso de red porque define la forma en que los datos se transmiten a través de líneas telefónicas.", useCases: "Acceso a Internet, comunicaciones digitales." },
+    { src: "/ethernet.png", alt: "Ethernet", definition: "Ethernet es una tecnología de red utilizada para la comunicación de datos en redes locales (LAN).", reason: "Pertenece a la capa de acceso de red porque define la forma en que los dispositivos en una red local se comunican.", useCases: "Redes locales, comunicación de datos.", ports: "Puerto no aplicable" },
+    { src: "/wifi.png", alt: "Wi-Fi", definition: "Wi-Fi es una tecnología de red inalámbrica que permite a los dispositivos comunicarse sin cables.", reason: "Pertenece a la capa de acceso de red porque define la comunicación inalámbrica en redes locales.", useCases: "Conexiones inalámbricas, redes domésticas.", ports: "Puerto no aplicable" },
+    { src: "/dsl.png", alt: "DSL", definition: "DSL es una tecnología que proporciona acceso a Internet mediante la transmisión de datos digitales a través de líneas telefónicas.", reason: "Pertenece a la capa de acceso de red porque define la forma en que los datos se transmiten a través de líneas telefónicas.", useCases: "Acceso a Internet, comunicaciones digitales.", ports: "Puerto no aplicable" },
   ],
 };
 
@@ -41,7 +40,7 @@ const EjemplosTCPIP = ({ layerTitle }) => {
       {imagenes.map((image, index) => (
         <div
           key={index}
-          className="p-2 w-full sm:w-1/2 lg:w-1/3 cursor-pointer relative flex flex-col items-center justify-center"
+          className="p-1 sm:p-2 w-full sm:w-1/2 lg:w-1/3 cursor-pointer relative flex flex-col items-center justify-center"
           onClick={() => handleImageClick(image)}
         >
           <CSSTransition
@@ -55,6 +54,7 @@ const EjemplosTCPIP = ({ layerTitle }) => {
                 <h2 className="text-xl font-bold mb-2">{image.alt}</h2>
                 <p className="mb-2">{image.definition}</p>
                 <p className="mb-2 font-bold">{image.reason}</p>
+                <p className="mb-2 text-xl">{image.ports}</p>
               </div>
             </div>
           </CSSTransition>
